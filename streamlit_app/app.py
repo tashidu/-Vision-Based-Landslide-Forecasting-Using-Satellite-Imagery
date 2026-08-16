52d8b21ec11e7f1411a5d813f6fe24ee582aa0d8
DANA2118 <thaksha-im21019@stu.kln.ac.lk>
2026-08-17 01:36:03 +0530

    feat: initialize Streamlit UI

---
+++ streamlit_app/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Landslide Forecasting",
+    page_icon="🛰️",
+    layout="wide"
+)
+
+
+st.title("🛰️ Landslide Forecasting")
+st.write(
+    "Vision-Based Landslide Detection Using Satellite Imagery"
+)
